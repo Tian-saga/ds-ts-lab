5b42f73f4ba4d6596c403040b050eb7496e74274
Parrot-zzt <13822593+parrot-zzt@user.noreply.gitee.com>
2025-02-07 17:14:29 +0000

    Added allOlder function to increment all friends' ages

--- src/02-functions.ts
+++ src/02-functions.ts
@@ -1,1 +1,16 @@
 import {Friend, Colleague } from './myTypes'
+import { friends } from "./01-basics";
+
+function older(f: Friend) : string {
+    f.age += 1
+    return `${f.name} is now ${f.age}` 
+}
+
+function allOlder(friendsArray: Friend[]): string[] {
+    return friendsArray.map(f => {
+      const newAge = f.age + 1; // 计算新年龄
+      return `${f.name} is now ${newAge}`;
+    });
+  }
+  console.log(older(friends[0]))
+  console.log(allOlder(friends));
